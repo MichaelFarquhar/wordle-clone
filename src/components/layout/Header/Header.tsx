281c3647a259f02902b1c6108c6c54299531cc22
Michael <michaelfarquhar4@gmail.com>
2022-05-10 21:22:10 -0400

    Add title and github link to header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import styles from './Header.module.css';
+import { FaGithub } from 'react-icons/fa';
 
 export const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div className={styles.header}>
+            <div>{/* Spacing to center the title */}</div>
+            <h1>Wordle</h1>
+            <a
+                href="https://github.com/MichaelFarquhar/wordle-clone"
+                target="blank"
+                rel="noopener noreferrer"
+            >
+                <FaGithub />
+            </a>
+        </div>
+    );
 };
